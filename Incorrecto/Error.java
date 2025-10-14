bd2e99323ea3bc1b214fd3639beff387a1fb4b23
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-10-14 12:45:15 -0500

    1.3

---
+++ Incorrecto/Error.java
@@ -0,0 +1,19 @@
+/*interface ErrorDefault {
+    default void correr() {
+        System.out.println("Hola");
+
+    }
+}
+
+interface Fallo {
+
+    void caminar();
+}
+
+abstract class FalloAbstracto {
+}
+
+public class Error implements Fallo {
+
+}
+*/
